6c774af343cf5c6209d0d068a9c60cda843ceb73
Obenpasaribu <142556157+Obenpasaribu@users.noreply.github.com>
2026-03-02 16:01:46 +0700

    in
    ok

--- src/main/kotlin/tables/PlantpcTable.kt
+++ src/main/kotlin/tables/PlantpcTable.kt
@@ -3,7 +3,7 @@ package org.delcom.tables
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
 
-object PlantpcTable : UUIDTable("plants") {
+object PlantpcTable : UUIDTable("plantspc") {
     val nama = varchar("nama", 100)
     val pathGambar = varchar("path_gambar", 255)
     val deskripsi = text("deskripsi")
