5c0cf06a21d32904239b9bc97aa003a61945a53f
Obenpasaribu <142556157+Obenpasaribu@users.noreply.github.com>
2026-04-08 14:46:02 +0700

    in

--- src/main/kotlin/tables/PlantTable.kt
+++ src/main/kotlin/tables/PlantTable.kt
@@ -8,7 +8,7 @@ object PlantTable : UUIDTable("plants") {
     val pathGambar = varchar("path_gambar", 255)
     val deskripsi = text("deskripsi")
     val manfaat = text("manfaat")
-    val efekSamping = text("efeksamping")
+    val efekSamping = text("efek_samping")
     val createdAt = timestamp("created_at")
     val updatedAt = timestamp("updated_at")
 }
